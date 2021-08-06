0393aa31131405ac8f8988577219c048c573db2e
Ynse Hoornenborg <github@ynse.nl>
2021-08-06 21:40:27 +0200

    Test failure scenarios too

--- Tests/Commands/CreateCommandTest.cs
+++ Tests/Commands/CreateCommandTest.cs
@@ -65,7 +65,6 @@ namespace Meel.Tests.Commands
         {
             // Arrange
             var station = new InMemoryStation();
-            var user = "Piet";
             var box = "Existing";
             var command = new CreateCommand(station);
             var response = new ImapResponse();
@@ -78,6 +77,54 @@ namespace Meel.Tests.Commands
             var txt = response.ToString();
             Assert.IsNotNull(txt);
             StringAssert.DoesNotContain("OK", txt);
+            StringAssert.DoesNotContain("NO", txt);
+            StringAssert.Contains("BAD", txt);
+        }
+
+        [Test]
+        public void ShouldNotCreateAfterLogout()
+        {
+            // Arrange
+            var station = new InMemoryStation();
+            var user = "Piet";
+            var box = "Existing";
+            var command = new CreateCommand(station);
+            var response = new ImapResponse();
+            var context = new ConnectionContext(42);
+            context.State = SessionState.Logout;
+            context.Username = user;
+            var requestId = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes("123"));
+            var options = box.AsAsciiSpan();
+            // Act
+            command.Execute(context, requestId, options, ref response);
+            // Assert
+            var txt = response.ToString();
+            Assert.IsNotNull(txt);
+            StringAssert.DoesNotContain("OK", txt);
+            StringAssert.DoesNotContain("NO", txt);
+            StringAssert.Contains("BAD", txt);
+        }
+
+        [Test]
+        public void ShouldReturnBadWithoutArgument()
+        {
+            // Arrange
+            var station = new InMemoryStation();
+            var user = "Piet";
+            var command = new CreateCommand(station);
+            var response = new ImapResponse();
+            var context = new ConnectionContext(42);
+            context.State = SessionState.Authenticated;
+            context.Username = user;
+            var requestId = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes("123"));
+            var options = "".AsAsciiSpan();
+            // Act
+            command.Execute(context, requestId, options, ref response);
+            // Assert
+            var txt = response.ToString();
+            Assert.IsNotNull(txt);
+            StringAssert.DoesNotContain("OK", txt);
+            StringAssert.DoesNotContain("NO", txt);
             StringAssert.Contains("BAD", txt);
         }
     }
