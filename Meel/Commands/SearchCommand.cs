d3f8b47236a2b1132cb9abdaf218d2d77740b8ca
Ynse Hoornenborg <github@ynse.nl>
2021-08-10 23:11:45 +0200

    Search command test and fixes

--- Meel/Commands/SearchCommand.cs
+++ Meel/Commands/SearchCommand.cs
@@ -10,6 +10,7 @@ namespace Meel.Commands
     {
         private static readonly byte[] completedHint = Encoding.ASCII.GetBytes("SEARCH completed");
         private static readonly byte[] searchHint = Encoding.ASCII.GetBytes("SEARCH");
+        private static readonly byte[] missingHint = Encoding.ASCII.GetBytes("No mailbox selected");
         private static readonly byte[] argsHint = Encoding.ASCII.GetBytes("Need to specify a query");
         private static readonly byte[] modeHint =
             Encoding.ASCII.GetBytes("Need to be in Selected mode for this command");
@@ -21,18 +22,29 @@ namespace Meel.Commands
             if (context.State == SessionState.Selected) {
                 if (!requestOptions.IsEmpty)
                 {
-                    var numMessages = context.SelectedMailbox.NumberOfMessages;
-                    var searchKey = SearchKeyParser.Parse(requestOptions, (uint)numMessages);
-                    // TODO: Specify UID mode
-                    var list = station.SearchMailbox(context.SelectedMailbox, searchKey, true);
-                    response.AppendLine(ImapResponse.Untagged, searchHint, string.Join(' ', list).AsAsciiSpan());
-                    response.AppendLine(requestId, ImapResponse.Ok, completedHint);
+                    if (context.SelectedMailbox != null)
+                    {
+                        var numMessages = context.SelectedMailbox.NumberOfMessages;
+                        var searchKey = SearchKeyParser.Parse(requestOptions, (uint)numMessages);
+                        // TODO: Specify UID mode
+                        var list = station.SearchMailbox(context.SelectedMailbox, searchKey, true);
+                        var listSpan = string.Join(' ', list).AsAsciiSpan();
+                        response.Allocate(12 + requestId.Length + completedHint.Length + listSpan.Length + searchHint.Length);
+                        response.AppendLine(ImapResponse.Untagged, searchHint, listSpan);
+                        response.AppendLine(requestId, ImapResponse.Ok, completedHint);
+                    } else
+                    {
+                        response.Allocate(6 + requestId.Length + missingHint.Length);
+                        response.AppendLine(requestId, ImapResponse.No, missingHint);
+                    }
                 } else
                 {
+                    response.Allocate(7 + requestId.Length + argsHint.Length);
                     response.AppendLine(requestId, ImapResponse.Bad, argsHint);
                 }
             } else
             {
+                response.Allocate(7 + requestId.Length + modeHint.Length);
                 response.AppendLine(requestId, ImapResponse.Bad, modeHint);
             }
             return 0;
---
+++ Tests/Commands/SearchCommandTest.cs
@@ -0,0 +1,137 @@
+﻿using Meel.Commands;
+using Meel.Parsing;
+using Meel.Responses;
+using Meel.Stations;
+using NUnit.Framework;
+using System.Buffers;
+using System.Text;
+
+namespace Meel.Tests.Commands
+{
+    [TestFixture]
+    public class SearchCommandTest
+    {
+        [Test]
+        public void ShouldSearchExistingBox()
+        {
+            // Arrange
+            var station = new InMemoryStation();
+            var user = "Piet";
+            var boxName = "Existing";
+            var query = "ALL";
+            station.CreateMailbox(user, boxName);
+            var box = station.SelectMailbox(user, boxName);
+            var command = new SearchCommand(station);
+            var response = new ImapResponse();
+            var context = new ConnectionContext(42);
+            context.State = SessionState.Selected;
+            context.Username = user;
+            context.SetSelectedMailbox(box);
+            var requestId = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes("123"));
+            var options = query.AsAsciiSpan();
+            // Act
+            command.Execute(context, requestId, options, ref response);
+            // Assert
+            var txt = response.ToString();
+            Assert.IsNotNull(txt);
+            StringAssert.DoesNotContain("BAD", txt);
+            StringAssert.DoesNotContain("NO", txt);
+            StringAssert.Contains("OK", txt);
+        }
+
+        [Test]
+        public void ShouldNotSearchNonExistingBox()
+        {
+            // Arrange
+            var station = new InMemoryStation();
+            var user = "Piet";
+            var query = "ALL";
+            var command = new SearchCommand(station);
+            var response = new ImapResponse();
+            var context = new ConnectionContext(42);
+            context.SetSelectedMailbox(null);
+            context.State = SessionState.Selected;
+            context.Username = user;
+            var requestId = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes("123"));
+            var options = query.AsAsciiSpan();
+            // Act
+            command.Execute(context, requestId, options, ref response);
+            // Assert
+            var txt = response.ToString();
+            Assert.IsNotNull(txt);
+            StringAssert.DoesNotContain("OK", txt);
+            StringAssert.DoesNotContain("BAD", txt);
+            StringAssert.Contains("NO", txt);
+        }
+
+        [Test]
+        public void ShouldNotSearchBeforeLogin()
+        {
+            // Arrange
+            var station = new InMemoryStation();
+            var user = "Piet";
+            var box = "Existing";
+            var command = new SearchCommand(station);
+            var response = new ImapResponse();
+            var context = new ConnectionContext(42);
+            context.Username = user;
+            var requestId = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes("123"));
+            var options = box.AsAsciiSpan();
+            // Act
+            command.Execute(context, requestId, options, ref response);
+            // Assert
+            var txt = response.ToString();
+            Assert.IsNotNull(txt);
+            StringAssert.DoesNotContain("OK", txt);
+            StringAssert.DoesNotContain("NO", txt);
+            StringAssert.Contains("BAD", txt);
+        }
+
+        [Test]
+        public void ShouldNotSearchAfterLogout()
+        {
+            // Arrange
+            var station = new InMemoryStation();
+            var user = "Piet";
+            var box = "Existing";
+            var command = new SearchCommand(station);
+            var response = new ImapResponse();
+            var context = new ConnectionContext(42);
+            context.State = SessionState.Logout;
+            context.Username = user;
+            var requestId = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes("123"));
+            var options = box.AsAsciiSpan();
+            // Act
+            command.Execute(context, requestId, options, ref response);
+            // Assert
+            var txt = response.ToString();
+            Assert.IsNotNull(txt);
+            StringAssert.DoesNotContain("OK", txt);
+            StringAssert.DoesNotContain("NO", txt);
+            StringAssert.Contains("BAD", txt);
+        }
+
+        [Test]
+        public void ShouldReturnBadWithoutArgument()
+        {
+            // Arrange
+            var station = new InMemoryStation();
+            var user = "Piet";
+            var command = new SearchCommand(station);
+            var response = new ImapResponse();
+            var context = new ConnectionContext(42);
+            context.State = SessionState.Selected;
+            context.Username = user;
+            var requestId = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes("123"));
+            var options = "".AsAsciiSpan();
+            // Act
+            command.Execute(context, requestId, options, ref response);
+            // Assert
+            var txt = response.ToString();
+            Assert.IsNotNull(txt);
+            StringAssert.DoesNotContain("OK", txt);
+            StringAssert.DoesNotContain("NO", txt);
+            StringAssert.Contains("BAD", txt);
+        }
+    }
+}
