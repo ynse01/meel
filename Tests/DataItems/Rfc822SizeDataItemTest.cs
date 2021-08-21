443b7d5f488dedde597ed9913923c2c8945f0ce2
Ynse Hoornenborg <github@ynse.nl>
2021-08-21 10:39:09 +0200

    RFC822 Size data item test

---
+++ Tests/DataItems/Rfc822SizeDataItemTest.cs
@@ -0,0 +1,26 @@
+﻿using Meel.DataItems;
+using Meel.Responses;
+using NUnit.Framework;
+
+namespace Meel.Tests.DataItems
+{
+    [TestFixture]
+    public class Rfc822SizeDataItemTest
+    {
+        [Test]
+        public void ShouldPrintSize()
+        {
+            // Arrange
+            var item = new Rfc822SizeDataItem();
+            var expected = "RFC822.SIZE 42";
+            var response = new ImapResponse();
+            var message = new ImapMessage(null, 1, MessageFlags.None, 42);
+            response.Allocate(1000);
+            // Act
+            item.PrintContent(ref response, message);
+            // Assert
+            var txt = response.ToString();
+            StringAssert.AreEqualIgnoringCase(expected, txt);
+        }
+    }
+}
